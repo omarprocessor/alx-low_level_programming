67270e3899eb6b9173c070ac2afefac561ffb50c
omarprocessor <wadaniinarais1@gmail.com>
2024-06-20 15:13:20 -0700

    add 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
-
+#include <unistd.h>
 /**
- * main - Entry point
- * Return: Always 1 (Success)
+ * main - Entry point of the program
+ *
+ * Description: This program prints a specific message to the standard output
+ * using the write() system call. It returns 1 to indicate success.
+ *
+ * Return: Always 1 (indicating success)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+		59);
 	return (1);
 }
+
