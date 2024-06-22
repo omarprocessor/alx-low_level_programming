0a358b40629d771d7166684debaf8848101def82
omarprocessor <wadaniinarais1@gmail.com>
2024-06-21 18:09:04 -0700

    correction of 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,12 +11,13 @@ int num1, num2;
 
 for (num1 = 0; num1 <= 8; num1++)
 {
-putchar(num1 + '0');
 for (num2 = num1 + 1; num2 <= 9; num2++)
 {
+putchar(num1 + '0');
 putchar(num2 + '0');
 
-if (num1 != 8 || num2 != 9)
+
+if (!(num1 == 8 && num2 == 9))
 {
 putchar(',');
 putchar(' ');
