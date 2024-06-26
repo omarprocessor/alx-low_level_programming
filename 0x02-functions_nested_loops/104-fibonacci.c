ae56ac123ead42dfa73ee816f0a0d10b4e0cfe2b
omarprocessor <wadaniinarais1@gmail.com>
2024-06-25 20:14:09 -0700

    Write a program that finds and prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line.
    The numbers should be separated by comma, followed by a space ,
    You are allowed to use the standard library
    You are not allowed to use any other library (You can’t use GMP etc…)
    You are not allowed to use long long, malloc, pointers, arrays/tables, or structures
    You are not allowed to hard code any Fibonacci number (except for 1 and 2)

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,25 +1,39 @@
 #include <stdio.h>
+#define MAX_DIGITS 10000000000
 /**
   * main - Prints the sum of Even Fibonacci numbers
   * less than 4000000.
   *
   * Return: Nothing!
   */
-int main() {
-int count = 98;
-int first = 1, second = 2, next;
+int main(void)
+{
+    unsigned long int a = 0, b = 1, c = 0, d = 2;
+    unsigned long int x, y, z;
+    int count;
 
-printf("%d, %d", first, second);
-
-for (int i = 3; i <= count; ++i) {
-next = first + second;
-printf(", %d", next);
-first = second;
-second = next;
-}
-
-printf("\n");
-
-return 0;
+    printf("%lu, %lu, ", b, d);
+    for (count = 2; count < 98; count++)
+    {
+        if (b + d > MAX_DIGITS || c > 0 || a > 0)
+        {
+            x = (b + d) / MAX_DIGITS;
+            y = (b + d) % MAX_DIGITS;
+            z = a + c + x;
+            a = c, c = z;
+            b = d, d = y;
+            printf("%lu%010lu", c, d);
+        }
+        else
+        {
+            y = b + d;
+            b = d, d = y;
+            printf("%lu", d);
+        }
+        if (count != 97)
+            printf(", ");
+    }
+    printf("\n");
+    return (0);
 }
 
