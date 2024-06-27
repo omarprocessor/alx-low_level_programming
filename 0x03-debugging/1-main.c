47b9f3db572537a4eb16373e97ac845398bb6e23
omarprocessor <wadaniinarais1@gmail.com>
2024-06-27 15:37:26 -0700

    Copy this main file and comment

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main -  an infinite loop
+* Return: Always 0
+*/
+
+int main(void)
+{
+printf("Infinite loop incoming :(\n");
+
+/* int i; */
+/* i = 0; */
+
+/* while (i < 10) */
+/* { */
+/* putchar(i); */
+/* i++; */
+/* } */
+
+printf("Infinite loop avoided! \\o/\n");
+
+return (0);
+}
+
