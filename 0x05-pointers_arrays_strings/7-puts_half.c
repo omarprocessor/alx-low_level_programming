cf7302e3647214690ce6aa7c787bffcff3573841
omarprocessor <wadaniinarais1@gmail.com>
2024-07-01 15:52:21 -0700

    Write a function that prints half of a string, followed by a new line.

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+* puts_half - Prints the second half of a string, followed by a new line.
+* @str: The string to process.
+*
+* Description: If the length of the string is odd, prints the last n
+* characters where n = (length_of_the_string - 1) / 2.
+* Return: void.
+*/
+void puts_half(char *str)
+{
+int length = 0;
+int i, start;
+
+while (str[length] != '\0')
+{
+length++;
+}
+
+if (length % 2 == 0)
+{
+start = length / 2;
+}
+else
+{
+start = (length - 1) / 2;
+}
+
+for (i = start; i < length; i++)
+{
+_putchar(str[i]);
+}
+
+_putchar('\n');
+}
+
