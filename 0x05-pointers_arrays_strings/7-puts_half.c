787ba34d25b159796dbdd148bcf56611142a5195
omarprocessor <wadaniinarais1@gmail.com>
2024-07-01 16:01:50 -0700

    correction of 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,37 +1,47 @@
 #include "main.h"
 
 /**
-* puts_half - Prints the second half of a string, followed by a new line.
-* @str: The string to process.
+*puts_half - Prints the second half of a string, followed by a new line.
+*@str: The string to process.
 *
-* Description: If the length of the string is odd, prints the last n
-* characters where n = (length_of_the_string - 1) / 2.
-* Return: void.
+*Description: If the length of the string is odd, prints the last n
+*characters where n = (length_of_the_string + 1) / 2.
+*Return: void.
 */
 void puts_half(char *str)
 {
-int length = 0;
-int i, start;
-
-while (str[length] != '\0')
-{
-length++;
-}
+int length = _strlen(str);
+int start_index;
 
 if (length % 2 == 0)
 {
-start = length / 2;
+start_index = length / 2;
 }
 else
 {
-start = (length - 1) / 2;
+start_index = (length + 1) / 2;
 }
 
-for (i = start; i < length; i++)
+while (str[start_index] != '\0')
 {
-_putchar(str[i]);
+_putchar(str[start_index]);
+start_index++;
+}
+_putchar('\n');
 }
 
-_putchar('\n');
+/**
+*_str_length - Computes the length of a string.
+*@s: The string to compute the length of.
+*Return: The length of the string.
+*/
+int _strlen(char *s)
+{
+int count = 0;
+while (s[count] != '\0')
+{
+count++;
+}
+return (count);
 }
 
