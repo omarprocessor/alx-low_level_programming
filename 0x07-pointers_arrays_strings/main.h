b0e349cc9b6e3e2d97734823951016518b69a86f
omarprocessor <wadaniinarais1@gmail.com>
2024-07-08 13:45:21 -0700

    Write a function that prints the chessboard.

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,7 +4,7 @@
 /* Standard Libraries */
 #include <stdio.h>
 #include <stdlib.h>
-
+int _putchar(char c);
 /* Function Prototypes */
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
