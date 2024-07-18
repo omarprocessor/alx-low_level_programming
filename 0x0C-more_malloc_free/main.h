f46981f014e4d3a49eee76f6c0d930a814de0bdc
omarprocessor <wadaniinarais1@gmail.com>
2024-07-18 14:04:52 -0700

    Write a function that allocates memory using malloc.

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+
+/* Function prototypes for Task 0 */
+void *malloc_checked(unsigned int b);
+
+/* Function prototypes for Task 1 */
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+/* Function prototypes for Task 2 */
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+/* Function prototypes for Task 3 */
+int *array_range(int min, int max);
+
+/* Function prototypes for Task 4 */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+/* Function prototypes for Task 5 */
+int _putchar(char c);
+
+#endif /* MAIN_H */
+
