f7a1d5c65b75decaf3bcc8715b4e111eabd3d6dd
omarprocessor <wadaniinarais1@gmail.com>
2024-08-06 14:21:20 -0700

    task 12 correction

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -5,7 +5,7 @@
 /**
  * print_listint_safe - Prints a listint_t linked list.
  * @head: Pointer to the head of the list.
- * 
+ *
  * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
