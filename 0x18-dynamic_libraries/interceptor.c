0e3fcfeeabf219d94b8caf266ce1098d0bf69722
omarprocessor <wadaniinarais1@gmail.com>
2024-09-23 13:18:08 -0700

    task 3 done

---
+++ 0x18-dynamic_libraries/interceptor.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void __attribute__((constructor)) init() {
+}
+
+int gm(int n1, int n2, int n3, int n4, int n5, int bonus) {
+    return (1); 
+}
